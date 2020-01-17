df9b0ac456b2bca86049f1469064a2bf735c521a
Matiasrpv <Matiasrpv1@gmail.com>
2020-01-17 10:27:28 -0300

    Se crean modelos de doctor y especialidades

--- clinicaDavila/clinica/models.py
+++ clinicaDavila/clinica/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from  django.urls import reverse
+import uuid
 # Create your models here.
+
+class Especialidad(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Doctor(models.Model):
+
+    rut = models.UUIDField(primary_key=True, help_text='Rut unico del doctors')
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    especialidad = models.ForeignKey('Especialidad', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.rut}, {self.nombre}, ({self.especialidad.name})'
+
+
