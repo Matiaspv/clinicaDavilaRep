a5fd33099a09fed8c9bfb22ae49479c6d9a1456d
matiaspv <matiasrpv1@gmail.com>
2020-01-18 23:20:26 -0300

    Se modifica archivo urls.py view.py  y settings y urls generales

--- clinicaDavila/clinica/views.py
+++ clinicaDavila/clinica/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import Doctor, Especialidad
 # Create your views here.
+
+def index(request):
+
+    num_Doctors = Doctor.objects.all().count()
+
+    num_Especiality = Especialidad.objects.all().count()
+
+
+    num_EspecialityNeuro = Especialidad.objects.filter(name='Neurocirujano').count()
+    
+    return render(
+        request,
+            'index.html',
+            context={'num_doctors':num_Doctors,'num_especiality':num_Especiality,'num_especialityneuro': num_EspecialityNeuro}
+    )
